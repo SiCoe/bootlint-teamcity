38ca108b95252974e3c1f87be15bbf606a3f580b
Simon Coe <sicoemail@googlemail.com>
2016-12-05 12:06:56 +0000

    skeleton of project

---
+++ lesshint-teamcity.js
@@ -0,0 +1,9 @@
+var tsm = require('teamcity-service-messages');
+
+var REPORTER = 'BootLint';
+
+module.exports = {
+    reportFn: (file, lint, isError, isWarning, errorLocation) => {
+        console.log('//ToDo: impliment reportFn');
+    }
+};
